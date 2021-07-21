c948e03f75e5b44be263a38566a484c582dc2b8f
Luksos9 <lukaszszumilas9@wp.pl>
2021-07-21 11:04:25 +0000

    Remove old stuff

--- portfolio_django/settings.py
+++ portfolio_django/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-n-xocc+*(th99y5&as^^@t-19ff4p8+dkva$3ldeu^x0c0w+k$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['luksos9.pythonanywhere.com']
 
 
 # Application definition
@@ -121,6 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
